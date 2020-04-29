19b3258a2a7bcec40c3d64bbee5c36ea9af01df3
jsg3121 <xodm95@gamil.com>
2020-04-29 17:28:43 +0900

    20200429-삽입정렬, 석차 구하기

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -7,21 +7,61 @@ import java.util.*;
 public class BubbleSort {
     public static void main(String[] args) {
         //버블 정렬
-        int[] arr = {8, 5, 6, 2, 4, 1, 3, 7, 9, 10};
+        int[] arr1 = {8, 5, 6, 2, 4, 1, 3, 7, 9, 10};
         int k = 0;
 
-        for(int i = 0; i < arr.length - 1; i++){
-            for(int j = 0; j < arr.length - 1; j++){
-                if(arr[j] > arr[j + 1]){
-                    k = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = k;
+        // 버블정렬 - 기본
+        System.out.println("버블정렬 - 기본");
+        System.out.println("--------------------------------");
+        
+        for(int i = 0; i < arr1.length - 1; i++){
+            for(int j = 0; j < arr1.length - 1; j++){
+                if(arr1[j] > arr1[j + 1]){
+                    k = arr1[j];
+                    arr1[j] = arr1[j + 1];
+                    arr1[j + 1] = k;
                 } 
             }
         }
-
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ", ");
+       
+        for(int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + ", ");
         }
+        System.out.println(" ");
+        System.out.println("--------------------------------");
+        System.out.println(" ");
+
+        // 버블정렬 - 중간 종료
+
+        System.out.println("버블정렬 - 중간 종료");
+        System.out.println("--------------------------------");
+
+        int[] arr2 = {8, 5, 6, 2, 4, 1, 3, 7, 9, 10};
+        int sw = 0;
+
+        for(int i = 0; i < arr2.length - 1; i++){
+            sw = 0;
+            for(int j = 0; j < arr2.length - 1; j++){
+                if(arr2[j] > arr2[j + 1]){
+                    k = arr2[j];
+                    arr2[j] = arr2[j + 1];
+                    arr2[j + 1] = k;
+                    sw = 1;
+                } 
+            }
+
+            if(sw == 0) {
+                System.out.println("회전 수 : " + i);
+                break;
+            }
+        }
+     
+        for(int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + ", ");
+        }
+        System.out.println(" ");
+        System.out.println("--------------------------------");
+        System.out.println(" ");
+
     }
 }
